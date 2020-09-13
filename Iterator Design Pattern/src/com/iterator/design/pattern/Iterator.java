dbad6539552d88135ac834591a0b2b8d0fe2d2c7
Nazhim <nazhimkalam@gmail.com>
2020-09-13 23:02:19 +0530

    completed Iterator Pattern

--- Iterator Design Pattern/src/com/iterator/design/pattern/Iterator.java
+++ Iterator Design Pattern/src/com/iterator/design/pattern/Iterator.java
@@ -1,10 +1,6 @@
 package com.iterator.design.pattern;
 
 
-// This pattern is used to get a way to access the elements of a collection object in sequential manner without
-//   any need to know its underlying representation.
-
-
 public interface Iterator {
     public boolean hasNext();
     public Object next();
--- Iterator Design Pattern/src/com/iterator/design/pattern/IteratorPatternDemo.java
+++ Iterator Design Pattern/src/com/iterator/design/pattern/IteratorPatternDemo.java
@@ -1,13 +1,19 @@
 package com.iterator.design.pattern;
 
+// This pattern is used to get a way to access the elements of a collection object in sequential manner without
+// any need to know its underlying representation.
+
 public class IteratorPatternDemo {
 
     public static void main(String[] args) {
         NameRepository namesRepository = new NameRepository();
 
         for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+
             String name = (String)iter.next();
             System.out.println("Name : " + name);
+
         }
     }
+
 }
